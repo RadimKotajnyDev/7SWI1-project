682fded98821ea04498bcdbb1d5770373e2a223c
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2026-02-18 12:43:25 +0100

    Chakra UI setup (#9)
    * add: chakraui package
    
    * add: recommended default snippets (chakraui)
    
    * update: format project
    
    * update: tsconfig.app.json based on chakraui installation guide
    
    * update: moved chakraui default components to shared
    
    * add: wrapped app with chakra provider
    
    * add: tsconfigPaths() to vite config
    
    * add: chakra cli script to package.json
    
    * update: bunx script - chakra snippet package.json
    
    * add: chakraprovider to main.tsx

--- src/Frontend/src/app/main.tsx
+++ src/Frontend/src/app/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider as ChakraProvider } from "@/shared/ui/provider.tsx";
 import App from "./App.tsx";
 
 const root = document.getElementById("root");
@@ -7,7 +8,9 @@ const root = document.getElementById("root");
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </StrictMode>,
   );
 }
