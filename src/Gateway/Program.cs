8efabfc0a78730abf4a60b6cdc3d430354a3b2e0
Filip Valentíny <filip.valentiny@itixo.com>
2026-05-03 11:08:52 +0200

    added allow credentials to CORS policy

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -13,7 +13,8 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
             .AllowAnyMethod()
-            .AllowAnyHeader();
+            .AllowAnyHeader()
+            .AllowCredentials();
     });
 });
 
