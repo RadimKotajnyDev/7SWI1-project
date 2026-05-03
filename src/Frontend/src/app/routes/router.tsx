782e15dced7c9169793d2a514240d2deb6b30398
Radim Kotajny <radimkotajny@gmail.com>
2026-05-03 13:18:26 +0200

    add: ROUTER paths

--- src/Frontend/src/app/routes/router.tsx
+++ src/Frontend/src/app/routes/router.tsx
@@ -1,22 +1,23 @@
 import { createBrowserRouter, Navigate } from "react-router";
 import { LoginPage } from "@/pages/login";
 import { RegisterPage } from "@/pages/register";
+import { ROUTES } from "./routes";
 
 export const router = createBrowserRouter([
   {
     index: true,
-    element: <Navigate to="/login" replace />,
+    element: <Navigate to={ROUTES.LOGIN.path} replace />,
   },
   {
-    path: "/login",
+    path: ROUTES.LOGIN.path,
     element: <LoginPage />,
   },
   {
-    path: "/register",
+    path: ROUTES.REGISTER.path,
     element: <RegisterPage />,
   },
   {
     path: "*",
-    element: <Navigate to="/login" replace />,
+    element: <Navigate to={ROUTES.LOGIN.path} replace />,
   },
 ]);
